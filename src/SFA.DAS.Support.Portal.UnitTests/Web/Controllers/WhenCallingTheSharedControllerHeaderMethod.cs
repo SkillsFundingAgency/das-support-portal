66d1154beaea9ffcf1079af8fbc3f6244342e4ee
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-11-29 16:54:36 +0000

    Created generic controller test base

---
+++ src/SFA.DAS.Support.Portal.UnitTests/Web/Controllers/WhenCallingTheSharedControllerHeaderMethod.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using SFA.DAS.Support.Portal.Web.Controllers;
+using SFA.DAS.Support.Portal.Web.ViewModels;
+
+namespace SFA.DAS.Support.Portal.UnitTests.Web.Controllers
+{
+    [TestFixture]
+    public class WhenCallingTheSharedControllerHeaderMethod : WhenTestingAController<SharedController>
+    {
+
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            _actionResultResponse = Unit.Header();
+        }
+
+        [Test]
+        public void ItShouldReturnAPartialHeaderView()
+        {
+            Assert.IsInstanceOf<PartialViewResult>(_actionResultResponse);
+
+            var expected = "_Header";
+            Assert.AreEqual(expected, (_actionResultResponse as PartialViewResult).ViewName);
+        }
+
+        [Test]
+        public void ItShouldReturnAUsernameInTheViewModel()
+        {
+            Assert.IsInstanceOf<HeaderViewModel>((_actionResultResponse as PartialViewResult).Model);
+            Assert.IsNotNull(((HeaderViewModel)(_actionResultResponse as PartialViewResult).Model).Username);
+        }
+    }
+}
---
+++ src/SFA.DAS.Support.Portal.UnitTests/Web/Controllers/WhenTestingAController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+
+namespace SFA.DAS.Support.Portal.UnitTests.Web.Controllers
+{
+    public class WhenTestingAController <T> where T: Controller
+    {
+        protected T Unit;
+        protected ActionResult _actionResultResponse;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            Unit = System.Activator.CreateInstance<T>();
+
+        }
+    }
+}
