681389371258d82ce4b7aa6fa0d53327cfd138cd
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-11-29 16:48:44 +0000

    Covering tests for the SharedController

---
+++ src/SFA.DAS.Support.Portal.UnitTests/Web/Controllers/SharedControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Web.Mvc;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Support.Portal.ApplicationServices.Queries;
+using SFA.DAS.Support.Portal.Web.Controllers;
+using SFA.DAS.Support.Portal.Web.Services;
+using SFA.DAS.Support.Portal.Web.ViewModels;
+
+namespace SFA.DAS.Support.Portal.UnitTests.Web.Controllers
+{
+    public class WhenTestingTheSharedController
+    {
+        protected SharedController _unit;
+        protected ActionResult _actionResult;
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            _unit = new SharedController();
+
+        }
+    }
+
+    [TestFixture]
+    public class WhenCallingTheHeaderMethod : WhenTestingTheSharedController
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            _actionResult = _unit.Header();
+        }
+
+        [Test]
+        public void ItShouldReturnAPartialHeaderView()
+        {
+            Assert.IsInstanceOf<PartialViewResult>(_actionResult);
+
+            var expected = "_Header";
+            Assert.AreEqual(expected, (_actionResult as PartialViewResult).ViewName);
+
+        }
+
+        [Test]
+        public void ItShouldReturntheCurrentUsernameInTheViewModel()
+        {
+
+
+
+            Assert.IsInstanceOf<HeaderViewModel>((_actionResult as PartialViewResult).Model);
+
+            Assert.IsNotNull(((HeaderViewModel)(_actionResult as PartialViewResult).Model).Username);
+
+
+
+
+        }
+    }
+}
