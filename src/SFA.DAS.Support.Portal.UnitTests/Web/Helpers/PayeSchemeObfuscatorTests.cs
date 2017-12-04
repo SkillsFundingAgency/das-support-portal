c6eae9478ed7014a4441b79db752a5f0ce883895
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-12-04 13:55:52 +0000

    Added PayeSchemeObfuscator Tests

---
+++ src/SFA.DAS.Support.Portal.UnitTests/Web/Helpers/PayeSchemeObfuscatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SFA.DAS.Support.Portal.Core.Helpers;
+
+namespace SFA.DAS.Support.Portal.UnitTests.Web.Helpers
+{
+    [TestFixture]
+    public class PayeSchemeObfuscatorTests
+    {
+        private PayeSchemeObfuscator _unit;
+        [SetUp]
+        public void Setup() { _unit = new PayeSchemeObfuscator();}
+
+        [TestCase("1/9", "1/9")]
+        [TestCase("ABC/DEFGHI", "A**/*****I")]
+        [TestCase("123/456789", "1**/*****9")]
+        public void ItShouldPass(string payeScheme, string expectedPayeScheme)
+        {
+            var actual = _unit.ObscurePayeScheme(payeScheme);
+            Assert.AreEqual(expectedPayeScheme, actual);
+        }
+    }
+}
