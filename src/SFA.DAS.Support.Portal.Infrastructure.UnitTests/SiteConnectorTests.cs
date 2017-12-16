67b4ec091a1a575ea93e4e4117faa66e0b7ccf89
Iain.Stevenson <Iain.Stevenson@live.co.uk>
2017-12-16 08:31:33 +0000

    Revised ChallengeController tests

--- src/SFA.DAS.Support.Portal.Infrastructure.UnitTests/SiteConnectorTests.cs
+++ src/SFA.DAS.Support.Portal.Infrastructure.UnitTests/SiteConnectorTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using RichardSzalay.MockHttp;
@@ -15,16 +14,6 @@ namespace SFA.DAS.Support.Portal.Infrastructure.UnitTests
     [TestFixture]
     public class SiteConnectorTests
     {
-        private MockHttpMessageHandler _mockHttpMessageHandler;
-        private HttpClient _httpClient;
-        private Uri _testUri;
-        private string _validTestResponseData;
-        private string _emptyJsonContent;
-        private TestType _testType;
-        private ISiteConnector _unit;
-        private string _testUrlMatch;
-        private static string _testUrl = "http://localhost/api/user/1234";
-
         [SetUp]
         public void Setup()
         {
@@ -38,42 +27,52 @@ namespace SFA.DAS.Support.Portal.Infrastructure.UnitTests
             _testUri = new Uri(_testUrl);
             _unit = new SiteConnector(_httpClient);
         }
-        [Test]
-        public async Task ItShouldUploadByUriAndFormDataSuccessfully()
+
+        private MockHttpMessageHandler _mockHttpMessageHandler;
+        private HttpClient _httpClient;
+        private Uri _testUri;
+        private string _validTestResponseData;
+        private string _emptyJsonContent;
+        private TestType _testType;
+        private ISiteConnector _unit;
+        private string _testUrlMatch;
+        private static string _testUrl = "http://localhost/api/user/1234";
+
+
+        [TestCase(HttpStatusCode.Ambiguous)] // 300
+        [TestCase(HttpStatusCode.BadRequest)] // 400
+        [TestCase(HttpStatusCode.Unauthorized)] // 401
+        [TestCase(HttpStatusCode.Conflict)] // 409
+        [TestCase(HttpStatusCode.ExpectationFailed)] // 417
+        [TestCase(HttpStatusCode.RequestedRangeNotSatisfiable)] // 416
+        [TestCase(HttpStatusCode.BadGateway)] // 502
+        public void ItShouldThrowAnExceptionWhenDownloadTypeRecievesHttpStatus(HttpStatusCode code)
         {
             _mockHttpMessageHandler
                 .When(_testUrlMatch)
-                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
+                .Respond(code, "application/json", _validTestResponseData)
+                ;
 
-            var formData = new Dictionary<string, string> {{"key", "value"}};
-            var response = await _unit.Upload<TestType>(_testUri, formData);
-
-            Assert.IsNotNull(response);
-
+            Assert.ThrowsAsync<HttpRequestException>(() => _unit.Download<TestType>(_testUri));
         }
-        [Test]
-        public async Task ItShouldDownloadTypeByUriSuccessfully()
+
+        [TestCase(HttpStatusCode.Ambiguous)] // 300
+        [TestCase(HttpStatusCode.BadRequest)] // 400
+        [TestCase(HttpStatusCode.Unauthorized)] // 401
+        [TestCase(HttpStatusCode.Conflict)] // 409
+        [TestCase(HttpStatusCode.ExpectationFailed)] // 417
+        [TestCase(HttpStatusCode.RequestedRangeNotSatisfiable)] // 416
+        [TestCase(HttpStatusCode.BadGateway)] // 502
+        public async Task ItShouldThrowAnExceptionWhenDownloadStringRecievesHttpStatus(HttpStatusCode code)
         {
             _mockHttpMessageHandler
                 .When(_testUrlMatch)
-                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
+                .Respond(code, "application/json", "{}")
+                ;
 
-            var response = await _unit.Download<TestType>(_testUri);
-
-            Assert.IsNotNull(response);
-            
+            Assert.ThrowsAsync<HttpRequestException>(() => _unit.Download(_testUrlMatch));
         }
-        [Test]
-        public async Task ItShouldDownloadTypeByUrlSuccessfully()
-        {
-            _mockHttpMessageHandler
-                .When(_testUrlMatch)
-                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
 
-            var response = await _unit.Download<TestType>(_testUrl);
-            Assert.IsNotNull(response);
-            Assert.IsInstanceOf<TestType>(response);
-        }
         [Test]
         public async Task ItShouldDownloadStringSuccessfully()
         {
@@ -86,64 +85,41 @@ namespace SFA.DAS.Support.Portal.Infrastructure.UnitTests
             Assert.IsInstanceOf<string>(response);
         }
 
-
-        [TestCase(HttpStatusCode.Ambiguous)] // 300
-        [TestCase(HttpStatusCode.BadRequest)] // 400
-        [TestCase(HttpStatusCode.Unauthorized)] // 401
-        [TestCase(HttpStatusCode.Conflict)] // 409
-        [TestCase(HttpStatusCode.ExpectationFailed)] // 417
-        [TestCase(HttpStatusCode.RequestedRangeNotSatisfiable)] // 416
-        [TestCase(HttpStatusCode.BadGateway)] // 502
-        public async Task ItShouldThrowAnExceptionWhenDownloadTypeRecievesHttpStatus(HttpStatusCode code)
+        [Test]
+        public async Task ItShouldDownloadTypeByUriSuccessfully()
         {
             _mockHttpMessageHandler
                 .When(_testUrlMatch)
-                .Respond(code, "application/json", _validTestResponseData)
-                ;
-            TestType response = null;
-            try
-            {
-                response = await _unit.Download<TestType>(_testUri);
-            }
-            catch (HttpRequestException e)
-            {
-                Assert.IsTrue(e.Message.StartsWith($"Response status code does not indicate success: {(int)code}"));
-            }
-            catch (Exception e)
-            {
-                Assert.Fail($"Exception of type {nameof(Exception)} thrown");
-            }
-            
+                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
+
+            var response = await _unit.Download<TestType>(_testUri);
+
+            Assert.IsNotNull(response);
         }
 
-        [TestCase(HttpStatusCode.Ambiguous)]// 300
-        [TestCase(HttpStatusCode.BadRequest)]// 400
-        [TestCase(HttpStatusCode.Unauthorized)] // 401
-        [TestCase(HttpStatusCode.Conflict)] // 409
-        [TestCase(HttpStatusCode.ExpectationFailed)] // 417
-        [TestCase(HttpStatusCode.RequestedRangeNotSatisfiable)] // 416
-        [TestCase(HttpStatusCode.BadGateway)] // 502
-        public async Task ItShouldThrowAnExceptionWhenDownloadStringRecievesHttpStatus(HttpStatusCode code)
+        [Test]
+        public async Task ItShouldDownloadTypeByUrlSuccessfully()
         {
             _mockHttpMessageHandler
-                 .When(_testUrlMatch)
-                 .Respond(code, "application/json", "{}")
-                 ;
+                .When(_testUrlMatch)
+                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
 
-            try
-            {
-                var response = await _unit.Download(_testUrlMatch);
-            }
-            catch (HttpRequestException e)
-            {
-                Assert.IsTrue(e.Message.StartsWith($"Response status code does not indicate success: {(int)code}"));
-            }
-            catch (Exception e)
-            {
-                Assert.Fail($"Exception of type {nameof(Exception)} thrown {e.Message}");
-            }
+            var response = await _unit.Download<TestType>(_testUrl);
+            Assert.IsNotNull(response);
+            Assert.IsInstanceOf<TestType>(response);
         }
 
+        [Test]
+        public async Task ItShouldUploadByUriAndFormDataSuccessfully()
+        {
+            _mockHttpMessageHandler
+                .When(_testUrlMatch)
+                .Respond(HttpStatusCode.OK, "application/json", _validTestResponseData);
 
+            var formData = new Dictionary<string, string> {{"key", "value"}};
+            var response = await _unit.Upload<TestType>(_testUri, formData);
+
+            Assert.IsNotNull(response);
+        }
     }
 }
--- src/SFA.DAS.Support.Portal.UnitTests/Web/Controllers/ChallengeController/WhenProvidingAnIncorrectChallengResponseToTheChallengeController.cs
+++ src/SFA.DAS.Support.Portal.UnitTests/Web/Controllers/ChallengeController/WhenProvidingAnIncorrectChallengResponseToTheChallengeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Principal;
+﻿using System.Collections.Generic;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -10,44 +11,147 @@ using SFA.DAS.Support.Portal.ApplicationServices;
 using SFA.DAS.Support.Portal.ApplicationServices.Queries;
 using SFA.DAS.Support.Portal.ApplicationServices.Responses;
 using SFA.DAS.Support.Portal.Core.Domain.Model;
+using SFA.DAS.Support.Portal.Web.Models;
 using SFA.DAS.Support.Portal.Web.Services;
 
 namespace SFA.DAS.Support.Portal.UnitTests.Web.Controllers.ChallengeController
 {
-    [TestFixture]
-    public class
-        WhenProvidingAnIncorrectChallengResponseToTheChallengeController : WhenTestingAnMvcControllerOfType<
-            Portal.Web.Controllers.ChallengeController>
+    public abstract class
+        WhenTestingTheChallengeController : WhenTestingAnMvcControllerOfType<Portal.Web.Controllers.ChallengeController>
     {
-        private Mock<IAccountRepository> _accountRepository;
-        private Mock<IGrantPermissions> _granter;
-        private Mock<IMediator> _mediator;
-        private Mock<HttpContextBase> _mockContextBase;
-        private ControllerContext _unitControllerContext;
+        protected Mock<IAccountRepository> AccountRepository;
+        protected Mock<IGrantPermissions> MockGranter;
+        protected Mock<IMediator> MockMediator;
+        protected Mock<HttpContextBase> MockContextBase;
+        protected ControllerContext UnitControllerContext;
+        protected Mock<HttpResponseBase> MockResponse;
+        protected Mock<IPrincipal> MockUser;
 
         [SetUp]
         public override void Setup()
         {
-            _granter = new Mock<IGrantPermissions>();
-            _accountRepository = new Mock<IAccountRepository>();
-            _mediator = new Mock<IMediator>();
+            MockGranter = new Mock<IGrantPermissions>();
+            AccountRepository = new Mock<IAccountRepository>();
+            MockMediator = new Mock<IMediator>();
 
             Unit = new Portal.Web.Controllers.ChallengeController(
-                _granter.Object,
-                _accountRepository.Object,
-                _mediator.Object);
+                MockGranter.Object,
+                AccountRepository.Object,
+                MockMediator.Object);
 
 
-            _mockContextBase = new Mock<HttpContextBase>();
+            MockContextBase = new Mock<HttpContextBase>();
 
-            _mockContextBase.Setup(x => x.Request).Returns(new Mock<HttpRequestBase>().Object);
-            _mockContextBase.Setup(x => x.Response).Returns(new Mock<HttpResponseBase>().Object);
-            _mockContextBase.Setup(x => x.User).Returns(new Mock<IPrincipal>().Object);
+            MockContextBase.Setup(x => x.Request).Returns(new Mock<HttpRequestBase>().Object);
+            MockResponse = new Mock<HttpResponseBase>();
+            MockContextBase.Setup(x => x.Response).Returns(MockResponse.Object);
+            MockUser = new Mock<IPrincipal>();
+            MockContextBase.Setup(x => x.User).Returns(MockUser.Object);
 
-            _unitControllerContext = new ControllerContext(_mockContextBase.Object, new RouteData(), Unit);
+            UnitControllerContext = new ControllerContext(MockContextBase.Object, new RouteData(), Unit);
 
-            Unit.ControllerContext = _unitControllerContext;
+            Unit.ControllerContext = UnitControllerContext;
         }
+    }
+    [TestFixture]
+    public class WhenReIssuingAChallenge : WhenTestingTheChallengeController
+    {
+        [Test]
+        public async Task ItShouldReturnTheChallengeView()
+        {
+            var id = "123";
+            var url = "https://tempuri.org/challenge";
+
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<ChallengeQuery>()))
+                .ReturnsAsync(new ChallengeResponse()
+                {
+                    Account = new Account(),
+                    StatusCode = SearchResponseCodes.Success,
+                    Characters = new List<int>() { 1, 2 }
+                });
+            var actual = await Unit.Index(id, url, true);
+
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOf<ViewResult>(actual);
+
+            Assert.IsInstanceOf<ChallengeViewModel>(((ViewResult)actual).Model);
+        }
+    }
+
+    [TestFixture]
+    public class WhenFirstIssuingAChallenge : WhenTestingTheChallengeController
+    {
+        [Test]
+        public async Task ItShouldReturnTheChallengeView()
+        {
+            var id = "123";
+            var url = "https://tempuri.org/challenge";
+
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<ChallengeQuery>()))
+                .ReturnsAsync(new ChallengeResponse()
+                {
+                    Account = new Account(),
+                    StatusCode = SearchResponseCodes.Success,
+                    Characters = new List<int>() { 1, 2 }
+                });
+            var actual = await Unit.Index(id, url, false);
+
+            Assert.IsNotNull(actual);
+            Assert.IsInstanceOf<ViewResult>(actual);
+
+            Assert.IsInstanceOf<ChallengeViewModel>(((ViewResult)actual).Model);
+        }
+    }
+
+    [TestFixture]
+    public class
+        WhenProvidingACorrectChallengResponseToTheChallengeController : WhenTestingTheChallengeController
+    {
+        [Test]
+        public void ItShouldRedirectWithoutAnError()
+        {
+            var challengeEntry = new ChallengeEntry
+            {
+                Balance = "100",
+                Challenge1 = "Challenge1",
+                Challenge2 = "Challenge2",
+                FirstCharacterPosition = "1",
+                SecondCharacterPosition = "2",
+                Id = "123123",
+                Url = "some/url"
+            };
+
+            var challengePermissionResponse = new ChallengePermissionResponse
+            {
+                Id = "123123",
+                IsValid = true,
+                Url = "some/url"
+            };
+
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<ChallengePermissionQuery>()))
+                .Returns(Task.FromResult(challengePermissionResponse));
+
+            MockContextBase.SetupGet(x => x.Request.CurrentExecutionFilePath).Returns("SomePathOrOther");
+
+            
+            ActionResultResponse = Unit.Index(challengeEntry).Result;
+
+            MockGranter.Verify(x => x.GivePermissions(MockResponse.Object, MockUser.Object, challengeEntry.Id)
+            , Times.Once);
+            
+            Assert.IsInstanceOf<RedirectResult>(ActionResultResponse);
+
+            var url = ((RedirectResult)ActionResultResponse).Url;
+
+            Assert.AreEqual(challengeEntry.Url, url);
+            Assert.IsFalse(url.Contains("hasError=true"));
+
+        }
+    }
+    [TestFixture]
+    public class
+        WhenProvidingAnIncorrectChallengResponseToTheChallengeController : WhenTestingTheChallengeController
+    {
         [Test]
         public void ItShouldRedirectWithAnError()
         {
@@ -69,14 +173,14 @@ namespace SFA.DAS.Support.Portal.UnitTests.Web.Controllers.ChallengeController
                 Url = "some/url"
             };
 
-            _mediator.Setup(x => x.SendAsync(It.IsAny<ChallengePermissionQuery>()))
+            MockMediator.Setup(x => x.SendAsync(It.IsAny<ChallengePermissionQuery>()))
                 .Returns(Task.FromResult(challengePermissionResponse));
-            _mockContextBase.Setup(x => x.Request.CurrentExecutionFilePath).Returns("SomePathOrOther");
-            // needs to mock 'Request.CurrentExecutionFilePath'
+            MockContextBase.Setup(x => x.Request.CurrentExecutionFilePath).Returns("SomePathOrOther");
+            
             ActionResultResponse = Unit.Index(challengeEntry).Result;
 
             Assert.IsInstanceOf<RedirectResult>(ActionResultResponse);
-            var url = ((RedirectResult) ActionResultResponse).Url;
+            var url = ((RedirectResult)ActionResultResponse).Url;
 
             Assert.AreEqual($"SomePathOrOther?url={challengeEntry.Url}&hasError=true", url);
         }
