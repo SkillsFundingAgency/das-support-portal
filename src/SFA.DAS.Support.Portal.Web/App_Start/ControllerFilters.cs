a4e0e31723016790445dc8414a8367c6bb82c725
kola-samuel <samuel.dev.dba@gmail.com>
2018-01-26 12:29:09 +0000

    IDAMS Authentication (#26)

--- src/SFA.DAS.Support.Portal.Web/App_Start/ControllerFilters.cs
+++ src/SFA.DAS.Support.Portal.Web/App_Start/ControllerFilters.cs
@@ -9,13 +9,10 @@ namespace SFA.DAS.Support.Portal.Web
     [ExcludeFromCodeCoverage]
     public class ControllerFilters
     {
-        private static readonly string AdfsMetadata = CloudConfigurationManager.GetSetting("ida_ADFSMetadata");
-
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             var roleSettings = DependencyResolver.Current.GetService<IRoleSettings>();
-            if (!Debugger.IsAttached && !string.IsNullOrWhiteSpace(AdfsMetadata))
-                filters.Add(new AuthorizeAttribute {Roles = roleSettings.ConsoleUserRole});
+            filters.Add(new AuthorizeAttribute { Roles = roleSettings.ConsoleUserRole });
         }
     }
 }
