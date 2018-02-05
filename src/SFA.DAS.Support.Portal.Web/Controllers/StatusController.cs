5aec4997b4c0d382bdb40167c007b7091eed3084
Iain Stevenson <31848657+IainStevenson@users.noreply.github.com>
2018-02-05 11:54:23 +0000

    Added Basic Secured Status Controller for remote service status validation (#36)

---
+++ src/SFA.DAS.Support.Portal.Web/Controllers/StatusController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace SFA.DAS.Support.Portal.Web.Controllers
+{
+    [System.Web.Mvc.RoutePrefix("api/status")]
+    public class StatusController : ApiController
+    {
+        // GET: Status
+        [System.Web.Mvc.AllowAnonymous]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(new
+            {
+                ServiceName = AddServiceName(),
+                ServiceTime = AddServerTime(),
+                Request = AddRequestContext()
+            });
+        }
+
+        private string AddRequestContext()
+        {
+            try
+            {
+                return $" {HttpContext.Current.Request.HttpMethod}: {HttpContext.Current.Request.RawUrl}";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+
+        private DateTimeOffset AddServerTime()
+        {
+            return DateTimeOffset.UtcNow;
+        }
+
+        private string AddServiceName()
+        {
+            try
+            {
+                return "SFA DAS Support Portal Site";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
+    }
+}
