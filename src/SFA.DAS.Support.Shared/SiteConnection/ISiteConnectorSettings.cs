d6deb4b139fd06d79b61e703414a723fa07e95d3
Iain Stevenson <31848657+IainStevenson@users.noreply.github.com>
2018-02-13 17:39:40 +0000

    Ascs 117 2 (#43)
    * Fix to Service configuration
    
    * Removed Config section
    
    * Removed redundnant Configuration values and classes

--- src/SFA.DAS.Support.Shared/SiteConnection/ISiteConnectorSettings.cs
+++ src/SFA.DAS.Support.Shared/SiteConnection/ISiteConnectorSettings.cs
@@ -2,7 +2,6 @@
 {
     public interface ISiteConnectorSettings
     {
-        string ApiBaseUrl { get; set; }
         string ClientId { get; set; }
         string ClientSecret { get; set; }
         string IdentifierUri { get; set; }
