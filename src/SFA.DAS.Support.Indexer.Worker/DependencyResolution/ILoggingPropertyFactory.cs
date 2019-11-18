e99f31e44ed8dea4ceee1576e8d02be65d185679
Dale Morson <dale.morson@gmail.com>
2019-11-18 12:48:56 +0000

    Removed SFA.DAS.Support.Indexer.Worker folder. Renamed SFA.DAS.Support.Portal.Indexer.Worker.UnitTests to SFA.DAS.Support.Portal.Indexer.Jobs.UnitTests.

--- src/SFA.DAS.Support.Indexer.Worker/DependencyResolution/ILoggingPropertyFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.Support.Indexer.Worker.DependencyResolution
-{
-    public interface ILoggingPropertyFactory
-    {
-        IDictionary<string, object> GetProperties();
-    }
-}
