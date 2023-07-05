f807e7e57f0b2ef6c4d510e384ed82dc5f21fc69
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-07-05 22:57:18 +0100

    package consolidation and remove unused test

--- src/SFA.DAS.Support.Shared.Tests/SiteConnector/UnAuthorisedSiteConnectorTests.cs
+++ src/SFA.DAS.Support.Shared.Tests/SiteConnector/UnAuthorisedSiteConnectorTests.cs
@@ -2,11 +2,10 @@
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
 using Moq;
 using Moq.Protected;
 using NUnit.Framework;
-using Microsoft.Azure.Services.AppAuthentication;
-using SFA.DAS.Support.Shared.SiteConnection;
 
 namespace SFA.DAS.Support.Shared.Tests.SiteConnector
 {
@@ -39,37 +38,6 @@ namespace SFA.DAS.Support.Shared.Tests.SiteConnector
 
             MockazureClientCredentialHelper
               .Verify(x => x.GetAccessTokenAsync(It.IsAny<string>()), Times.AtLeastOnce);
-        }
-
-        [Test]
-        public async Task GivenClientAuthIsSuppliedItShouldRequestATokenForUnAuthorisedClientsUsingAD()
-        {
-            MockHttpMessageHandler
-                .Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync((HttpRequestMessage request, CancellationToken token) => new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(ValidTestResponseData) });
-
-            var azureServiceTokenProvider = new Mock<AzureServiceTokenProvider>();
-
-            HttpClient.DefaultRequestHeaders.Authorization = null;
-
-            Unit = new SiteConnection.SiteConnector(HttpClient,
-              MockClientAuthenticator.Object,
-              Handlers,
-              MockLogger.Object,
-              MockazureClientCredentialHelper.Object);
-
-            var response = await Unit.Download(new Uri(TestUrl), TestResourceIdentifier);
-
-            Assert.IsNotNull(response);
-            Assert.IsInstanceOf<string>(response);
-            Assert.IsNotNull(HttpClient.DefaultRequestHeaders.Authorization);
-
-            MockazureClientCredentialHelper
-              .Verify(x => x.GetAccessTokenAsync(It.IsAny<string>()), Times.Never);
-
-            MockClientAuthenticator
-                .Verify(x => x.Authenticate(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
         }
     }
 }
