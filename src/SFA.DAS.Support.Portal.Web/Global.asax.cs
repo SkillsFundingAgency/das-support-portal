beb818c52d6e00b12d731d210e76bd33d05f5eca
Corey <corey.faulconbridge@digital.education.gov.uk>
2023-07-05 22:10:12 +0100

    remove unused code

--- src/SFA.DAS.Support.Portal.Web/Global.asax.cs
+++ src/SFA.DAS.Support.Portal.Web/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Web;
 using System.Web.Configuration;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -26,7 +25,6 @@ namespace SFA.DAS.Support.Portal.Web
             SetupApplicationInsights();
 
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             ControllerFilters.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
